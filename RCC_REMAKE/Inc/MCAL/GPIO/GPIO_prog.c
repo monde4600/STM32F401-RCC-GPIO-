32ad7d3a3dbb37e20e61881a25c839efbe38f277
Abanob Ade <104032534+monde4600@users.noreply.github.com>
2022-12-12 13:05:41 +0200

    Update GPIO_prog.c

--- RCC_REMAKE/Inc/MCAL/GPIO/GPIO_prog.c
+++ RCC_REMAKE/Inc/MCAL/GPIO/GPIO_prog.c
@@ -1,26 +1,22 @@
 /*
  * GPIO_prog.c
- *
  *  Created on: Dec 11, 2022
- *      Author: abano
+ *  Author: abano
  */
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
 #include "GPIO_config.h"
 #include "GPIO_private.h"
-
 void MGPIO_vSetPinMode(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum ,GPIO_MODE_t Copy_enuMode  )
 {
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->MODER &=~((_MODE_ANALOG)<<(Copy_enuPinNum*2));
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->MODER |=(Copy_enuMode)<<(Copy_enuPinNum*2);
 }
-
 void MGPIO_vSetPinOutPutType(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum ,GPIO_OUT_t Copy_enuOutPutType  )
 {
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->OTYPER &=~(OPEN_DRAIN<<Copy_enuPinNum);
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->OTYPER |= Copy_enuOutPutType<<Copy_enuPinNum;
 }
-
 void MGPIO_vSetPinOutSpeed(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum ,GPIO_SPEED_t Copy_enuOutSpeed )
 {
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->OSPEEDR &=~((_OUTPUT_SPEED_VERY_HIGH)<<(Copy_enuPinNum*2));
@@ -31,12 +27,10 @@ void MGPIO_vSetPullType(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum ,GPIO_PUL
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->PUPDR &=~((_PULL_reserved)<<(Copy_enuPinNum*2));
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->PUPDR |=(Copy_enuPullType)<<(Copy_enuPinNum*2);
 }
-
 GPIO_PIN_STATE_t MGPIO_enuReadData(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum)
 {
 	return GET_BIT((GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->IDR,Copy_enuPinNum);
 }
-
 void MGPIO_vWriteData(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum,GPIO_PIN_STATE_t Copy_enuState)
 {
 	if(Copy_enuState==_HIGH)
@@ -48,7 +42,6 @@ void MGPIO_vWriteDataDIR(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum ,GPIO_PI
 {
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->BSRR = 1<<(Copy_enuPinNum+16*(!Copy_enuState));
 }
-
 void MGPIO_vPinLock(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum )
 {
 	u32 Local_u16Handeller = (1 << LCKK) |(1 << Copy_enuPinNum);
@@ -57,7 +50,6 @@ void MGPIO_vPinLock(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum )
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->LCKR = Local_u16Handeller;
 	while(!GET_BIT((GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->LCKR ,LCKK));
 }
-
 void MGPIO_vSetPinAltFn(u8 Copy_enuPortName ,GPIO_PIN_t Copy_enuPinNum, GPIO_ALTFN_MOOD_t Copy_enuALF )
 {
 	(GPIO_BASE_ADDRES_TOT + Copy_enuPortName)->AFR[Copy_enuPinNum/8] &= ~(_ALTFN_15<<(Copy_enuPinNum*4)%31);
